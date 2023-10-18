7a4ee159fe4da893464b9a7122b82adb84ec939e
Bohdan Shkaran <bohdan.shkaran@todor3d.tech>
2023-10-18 19:40:59 +0300

    Close modal clicking outside

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -3,8 +3,8 @@ import "./style.scss";
 
 const Modal = ({ children, heading, setIsOpenedModal }) => {
     return (
-        <div className="modal">
-            <div className="modal__window">
+        <div className="modal"  onClick={() => setIsOpenedModal(false)}>
+            <div className="modal__window" onClick={(e) => e.stopPropagation()}>
                 <div className="modal__header">
                     <h4 className="modal__title">{heading}</h4>
                     <button className="modal__btn modal__btn--close"  onClick={() => setIsOpenedModal(false)}>
