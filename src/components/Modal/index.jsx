ae0360637979d0c40dfe48f6e8f76aac1f4ce836
Bohdan Shkaran <bohdan.shkaran@todor3d.tech>
2023-10-19 12:44:28 +0300

    Animate modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,34 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import Icon from "../Icon";
 import "./style.scss";
 
 const Modal = ({ children, heading, setIsOpenedModal }) => {
+    const modalWindowRef = useRef(null);
 
     useEffect(() => {
         document.body.style.overflow = 'hidden';
-
+        setTimeout(() => modalWindowRef.current.classList.add('modal__window--open'), 1);
         return () => {
             document.body.style.overflow = 'unset';
         }
     })
 
+    const close = () => {
+        modalWindowRef.current.classList.remove('modal__window--open');
+        setTimeout(() => setIsOpenedModal(false), 300);
+    }
+
     return (
-        <div className="modal"  onClick={() => setIsOpenedModal(false)}>
-            <div className="modal__window" onClick={(e) => e.stopPropagation()}>
-                <div className="modal__header">
-                    <h4 className="modal__title">{heading}</h4>
-                    <button className="modal__btn modal__btn--close"  onClick={() => setIsOpenedModal(false)}>
-                        <Icon
-                            name="modal-close"
-                            color="white"
-                            size={21}
-                        />
-                    </button>
+        <div className="modal" onClick={close}>
+            <div className="modal__window" ref={modalWindowRef} onClick={(e) => e.stopPropagation()}>
+                    <div className="modal__header">
+                        <h4 className="modal__title">{heading}</h4>
+                        <button className="modal__btn modal__btn--close"  onClick={close}>
+                            <Icon
+                                name="modal-close"
+                                color="white"
+                                size={21}
+                            />
+                        </button>
+                    </div>
+                    <div className="modal__body">
+                        {children}
+                    </div>
                 </div>
-                <div className="modal__body">
-                    {children}
-                </div>
-            </div>
         </div>
     )
 }
